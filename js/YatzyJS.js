34b7173f0d68917f1ddcd98a8c0c9343fc6af1c7
Daniel Koefoed <dc.koefoed@gmail.com>
2020-09-28 15:45:32 +0200

    third try

--- js/YatzyJS.js
+++ js/YatzyJS.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   let game = new Game();
-  
+
   let spel1_1 = document.getElementById("Ettor_1");
   let spel1_2 = document.getElementById("Tvåor_1");
   let spel1_3 = document.getElementById("Treor_1");
@@ -8,31 +8,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let spel1_5 = document.getElementById("Femmor_1");
   let spel1_6 = document.getElementById("Sexor_1");
  
-  spel1_1.addEventListener("change", function()
-  {
-    game.summa();
-
-    
-  });
-  spel1_2.addEventListener("change", function()
-  {
-    game.summa();
-    
-  });
-  spel1_3.addEventListener("change", function()
-  {
+  spel1_1.addEventListener("change", function () {
     game.summa();
   });
-  spel1_4.addEventListener("change", function()
-  {
+  spel1_2.addEventListener("change", function () {
     game.summa();
   });
-  spel1_5.addEventListener("change", function()
-  {
+  spel1_3.addEventListener("change", function () {
     game.summa();
   });
-  spel1_6.addEventListener("change", function()
-  {
+  spel1_4.addEventListener("change", function () {
+    game.summa();
+  });
+  spel1_5.addEventListener("change", function () {
+    game.summa();
+  });
+  spel1_6.addEventListener("change", function () {
     game.summa();
   });
   
@@ -47,24 +38,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
   {
     game.summa2();
   });
-  spel2_2.addEventListener("change", function()
-  {
+  spel2_2.addEventListener("change", function () {
     game.summa2();
   });
-  spel2_3.addEventListener("change", function()
-  {
+  spel2_3.addEventListener("change", function () {
     game.summa2();
   });
-  spel2_4.addEventListener("change", function()
-  {
+  spel2_4.addEventListener("change", function () {
     game.summa2();
   });
-  spel2_5.addEventListener("change", function()
-  {
+  spel2_5.addEventListener("change", function () {
     game.summa2();
   });
-  spel2_6.addEventListener("change", function()
-  {
+  spel2_6.addEventListener("change", function () {
     game.summa2();
   });
   
@@ -87,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     dice.addImage();
   });
   let btnSubmit = document.getElementById("toScoreboard");
-  btnSubmit.addEventListener("click", function (e) {    
+  btnSubmit.addEventListener("click", function (e) {
     dice.reset();
     dice = new Dice();
   });
